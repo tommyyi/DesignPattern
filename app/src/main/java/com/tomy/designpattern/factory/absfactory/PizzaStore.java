eca45d0f1567cb35b175970facd2dfa7b9e27093
teamo20150201@gmail.com <teamo20150201@gmail.com>
2017-07-30 16:14:25 +0800

    init

--- app/src/main/java/com/tomy/designpattern/factory/absfactory/PizzaStore.java
+++ app/src/main/java/com/tomy/designpattern/factory/absfactory/PizzaStore.java
@@ -1,6 +1,8 @@
 package com.tomy.designpattern.factory.absfactory;
 
 
+import jdk.nashorn.internal.runtime.Debug;
+
 public class PizzaStore
 {
     public static void main(String[] args)
--- app/src/main/java/com/tomy/designpattern/factory/simplefactory/PizzaStroe.java
+++ app/src/main/java/com/tomy/designpattern/factory/simplefactory/PizzaStroe.java
@@ -5,7 +5,6 @@ public class PizzaStroe
 {
     public static void main(String[] args)
     {
-        SimplePizzaFactory mSimplePizzaFactory;
         OrderPizza mOrderPizza;
         mOrderPizza = new OrderPizza(new SimplePizzaFactory());
 
